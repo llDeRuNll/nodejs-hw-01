6369a5d7c41e121f5878bb96a4e16bf99e09d3bb
llDeRuNll <eduardprudiys@gmail.com>
2025-06-05 21:06:38 -0400

    three functions

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const prevContacts = await readContacts();
+  const newContact = createFakeContact();
+  await writeContacts([...prevContacts, newContact]);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = Array(number).fill(0).map(createFakeContact);
+  const prevContacts = await readContacts();
+  await writeContacts([...prevContacts, ...contacts]);
+};
 
 generateContacts(5);
